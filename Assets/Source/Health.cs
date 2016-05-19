57079f4b28b5dda4d59fecfa8df14798f066e14d
WeissGz <rafagda@live.com>
2016-05-19 01:31:21 -0700

    Robot_Game

---
+++ Assets/Source/Health.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Health : MonoBehaviour, IDamageable
+{
+    public int MaxHealth;
+    [SerializeField]
+    private int CurrentHealth;
+
+    public void TakeDamage(int damage)
+    {
+        CurrentHealth -= damage;
+    }
+
+    // Use this for initialization
+    void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
---
+++ Assets/Source/ProjectileCollision.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileCollision : MonoBehaviour
+{
+    public EventHandler OnCollision;
+
+    private Collision m_ActiveCollision;
+    public Collision ActiveCollision { get { return m_ActiveCollision; } }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (OnCollision != null)
+        {
+            OnCollision(this, null);
+            IDamageable damageable = collision.collider.GetComponent<IDamageable>();
+            if (damageable != null)
+            {
+                damageable.TakeDamage(5);
+            }
+        }
+    }
+}
+
